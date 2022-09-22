ab4f5f70878cb1363639ac1eea503fd656d3d2f4
Rudra Vaibhav <106556982+rudra-101@users.noreply.github.com>
2022-09-22 21:29:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+//Dectecting Button Press
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButtons;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      var buttonInnerHTML=this.innerHTML;
+      makeSound(buttonInnerHTML);
+
+    }); // when addEventListener detects a click then it will this ananonimus functon which pop's out an alert msg
+}
+
+//Dectecting Key press
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key); // it will which key was pressed
+
+});
+//
+//
+function makeSound(key){
+    switch (key) {
+      case "w":
+        var tom1 = new Audio('tom-1.mp3');
+        tom1.play();
+      break;
+
+      case "a":
+        var tom2 = new Audio('tom-2.mp3');
+        tom2.play();
+      break;
+
+      case "s":
+        var tom3 = new Audio('tom-3.mp3');
+        tom3.play();
+      break;
+
+      case "d":
+        var tom4 = new Audio('tom-4.mp3');
+        tom4.play();
+      break;
+
+      case "j":
+        var snare = new Audio('snare.mp3');
+        snare.play();
+      break;
+
+      case "k":
+        var crash = new Audio('crash.mp3');
+        crash.play();
+      break;
+
+      case "l":
+        var kickBass = new Audio('kick-bass.mp3');
+        kickBass.play();
+      break;
+
+
+      default:
+
+    }
+}
